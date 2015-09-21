ee3db2b45e71ec59fe325fa0853faec54632e85b
helloco <hizhenghang@gmail.com>
2015-09-21 22:23:00 +0800

    second day

---
+++ controller/testController.class.php
@@ -0,0 +1,9 @@
+<?php
+class Test {
+	public function __construct() {
+		echo 'cons';
+	}
+	public function defaultAction() {
+		echo 'default action in test class';
+	}
+}
--- index.php
+++ index.php
@@ -6,33 +6,36 @@ since :2015-9-20
 */
 $uri = $_SERVER['REQUEST_URI'];
 $url = parse_url($uri);
-define('APP_PATH' ,$url['path']);
-define('CONTROLLER_NAME', 'c');
-define('METHOD_NAME', 'm');
+define('MY_APP_PATH', dirname(__FILE__));
+define('MY_CONTROLLER_NAME', 'c');
+define('MY_METHOD_NAME', 'm');
+define('MY_CONTROLLER_DIR', MY_APP_PATH . '\controller');
 
-$queryStrings = explode('&', $url['query']);
-// var_dump($queryStrings);exit;
-
-$germination = new Germination($queryStrings);
+$germination = new Mygermination($url);
 $germination->run();
 
 
-class Germination {
-	public $controller;
-	public $method;
-
+class Mygermination {
+	public $controller = 'default';
+	public $method = 'default';
+	// 请求参数
 	public $requestParameters = array();
 
-	public function __construct($queryStrings) {
-		foreach ($queryStrings as $key => $queryString) {
-			$query = explode("=", $queryString);
-			if ($query[0] == CONTROLLER_NAME && isset($query[1])) {
-				$this->setController($query[1]);
-			}
-			if ($query[0] == METHOD_NAME && isset($query[1])) {
-				$this->setMethod($query[1]);
-			}elseif ($query[0] != CONTROLLER_NAME && $query[0] != METHOD_NAME) {
-				$this->setParameter($query[0] , $query[1]);
+	public function __construct($url) {
+
+		if (!empty($url['query'])) {
+			$queryStrings = explode('&', $url['query']);
+			//var_dump($queryStrings);
+			foreach ($queryStrings as $key => $queryString) {
+				$query = explode("=", $queryString);
+				if ($query[0] == MY_CONTROLLER_NAME && isset($query[1])) {
+					$this->setController($query[1]);
+				}
+				if ($query[0] == MY_METHOD_NAME && isset($query[1])) {
+					$this->setMethod($query[1]);
+				}elseif ($query[0] != MY_CONTROLLER_NAME && $query[0] != MY_METHOD_NAME) {
+					$this->setParameter($query[0] , $query[1]);
+				}
 			}
 		}
 	}
@@ -58,8 +61,24 @@ class Germination {
 		return $this->requestParameters;
 	}
 
+	public function formatController () {
+		$controller = $this->getController();
+		return $controller . 'Controller.class.php';
+	}
+	
 	public function run() {
-		echo "<pre>";
-		var_dump($this->getController() , '-' , $this->getMethod(),'+',$this->getParameter());
+		// echo "<pre>";
+		// var_dump($this->getController() , '-' , $this->getMethod(),'+',$this->getParameter());
+		//echo MY_CONTROLLER_DIR . '\\' . $this->formatController();
+		$fileName = MY_CONTROLLER_DIR . '\\' . $this->formatController();
+		// echo $fileName;
+		//require CONTROLLER_DIR . '\' . $this->formatController();
+		require_once $fileName;
+		$className = ucfirst($this->getController());
+		$currenctClass = new $className;
+		$actionName = $this->getMethod() . 'Action()';
+		var_dump($actionName);
+		//$currenctClass->defaultAction();
+		//$currenctClass->
 	}
 }
